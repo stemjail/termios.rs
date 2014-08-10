09b9b97ae25be8030eda071a173cee8370fdc6fc
Nathan Zadoks <nathan@nathan7.eu>
2014-08-10 20:16:12 +0200

    initial commit

---
+++ implementors/core/fmt/trait.Show.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['lib'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='lib/struct.ControlCharacters.html' title='lib::ControlCharacters'>ControlCharacters</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='type' href='lib/type.Speed.html' title='lib::Speed'>Speed</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='struct' href='lib/struct.ControlCharacters.html' title='lib::ControlCharacters'>ControlCharacters</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/fmt/trait.Show.html' title='core::fmt::Show'>Show</a> for <a class='type' href='lib/type.Speed.html' title='lib::Speed'>Speed</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
