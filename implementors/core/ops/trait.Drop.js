18b5fc0ec2831f474afcb83c044a0e8643a1df98
Nathan Zadoks <nathan@nathan7.eu>
2014-08-11 12:15:38 +0200

    v1.1.1

--- implementors/core/ops/trait.Drop.js
+++ implementors/core/ops/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['termios'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='termios/struct.TermioHandle.html' title='termios::TermioHandle'>TermioHandle</a>&lt;'a&gt;",];
+implementors['termios'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/core/ops/trait.Drop.html' title='core::ops::Drop'>Drop</a> for <a class='struct' href='termios/struct.TermioHandle.html' title='termios::TermioHandle'>TermioHandle</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
